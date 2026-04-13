4e45da913a40835560efe247c617587ad4440909
zybbera <101921226+puski2003@users.noreply.github.com>
2026-04-13 23:44:50 +0530

    counselor application

--- migrations/add_meet_space_name.sql
+++
@@ -1,6 +0,0 @@
--- Migration: add meet_space_name to sessions
--- Run once against your database.
-
-ALTER TABLE sessions
-    ADD COLUMN meet_space_name VARCHAR(255) NULL
-        AFTER meeting_link;
